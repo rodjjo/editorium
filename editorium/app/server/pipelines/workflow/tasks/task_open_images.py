1e7deef2671beff1cc2d68a15d6f1df360a22f7f
Rodrigo Antonio de Araujo <rodjjo@gmail.com>
2024-10-27 06:33:49 -0300

    Option to open a random image

--- editorium/app/server/pipelines/workflow/tasks/task_open_images.py
+++ editorium/app/server/pipelines/workflow/tasks/task_open_images.py
@@ -1,3 +1,4 @@
+import random
 from .task import WorkflowTask
 from PIL import Image, ImageFilter, ImageOps
 
@@ -6,6 +7,7 @@ from pipelines.common.task_result import TaskResult
 
 class OpenImageSchema(Schema):
     prompt = fields.Str(required=True)
+    randomize = fields.Bool(required=False, load_default=False)
     globals = fields.Dict(required=False, load_default={})
 
 
@@ -25,8 +27,12 @@ class OpenImagesTask(WorkflowTask):
     def process_task(self, base_dir: str, name: str, input: dict, config: dict, callback: callable) -> dict:
         print("Processing open-images task")
         params = OpenImageSchema().load(config)
+        randomize = params.get('randomize', False)
         paths = params['prompt'].split('\n')
         paths = [p.strip() for p in paths if p.strip() != '']
+        if randomize:
+            random_index = random.randint(0, len(paths) - 1)
+            paths = [paths[random_index]]
         image_list = [Image.open(image.strip()) for image in paths]
         return TaskResult(image_list, paths).to_dict()
 
