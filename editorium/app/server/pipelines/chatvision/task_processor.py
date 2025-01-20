02d89845c87d36ffa63b0378d3cd7e15e135a153
Rodrigo Antonio de Araujo <rodjjo@gmail.com>
2025-01-20 13:13:02 -0300

    Improve vision task

--- editorium/app/server/pipelines/chatvision/task_processor.py
+++ editorium/app/server/pipelines/chatvision/task_processor.py
@@ -1,6 +1,7 @@
-from typing import List
+from typing import List, Dict
 
 import os
+import json
 import torch
 
 from pipelines.chatvision.managed_model import chatvision_model
@@ -28,89 +29,71 @@ def generate_text(repo_id: str,
     if type(image) is not list:
         image = [image]
     
-    context_dict = {}
+    
     prompt_texts = []
     prompt = prompt.split('\n')
-    prompt = [p.strip() for p in prompt if p]
-    text = ''
+    prompt = [p.strip() for p in prompt if p] 
+    prompt.append('next-turn:')
+    context_dict = {}
+    prompt_texts = []
+    in_turn = True
+    in_context = False
+    turn_text = ''
+    context_text = ''
     context_name = ''
-    in_context_capture = True
-    for p in prompt:
-        p = p.strip()
-        if p == '':
-            continue
-        if in_context_capture and p.lower().startswith('context:'):
+    for line in prompt:
+        line = line.strip()
+        if line.lower().startswith('context:'):
+            if in_turn:
+                if turn_text:
+                    prompt_texts.append(turn_text)
+                    turn_text = ''
+            in_context = True
+            in_turn = False
             if context_name:
-                context_dict[context_name] = text
-                text = ''
-            context_name = p.split(':', 1)[1].strip().lower()
-            if not context_name:
-                context_name = 'default'
-        elif p.lower() == 'next-turn:':
-            in_context_capture = False
-            if not text:
+                context_dict[context_name] = context_text
+                context_text = ''
                 context_name = ''
-                continue
+            context_name = line.lower().split(':')[1].strip()
+            continue
+
+        if line.lower() == 'next-turn:':
+            if in_turn:
+                if turn_text:
+                    prompt_texts.append(turn_text)
+                    turn_text = ''
+            in_turn = True
+            in_context = False
             if context_name:
-                context_dict[context_name] = text
-            else:
-                prompt_texts.append(text)
-            context_name = ''
-            text = ''
-        else:
-            text += p + '\n'
-    if text:
-        if context_name:
-            context_dict[context_name] = text
-        else:
-            prompt_texts.append(text)
-    
+                context_dict[context_name] = context_text
+                context_text = ''
+                context_name = ''
+
+            continue
+
+        if in_context:
+            context_text += '\n' + line
+            continue
+        if in_turn:
+            turn_text += '\n' + line
+   
     message_texts = []    
     for p in prompt_texts:
-        while '<context:' in p:
-            start = p.index('<context:')
-            end = p.index('>', start)
-            if end == -1:
-                print('Error: missing ">" ')
-                break
-            context_name = p[start + 9:end].lower()
-            if context_name in context_dict:
-                p = p[:start] + context_dict[context_name] + p[end + 1:]
-            else:
-                p = p[:start] + p[end + 1:]
         role = 'user'
         user_text = ''
         assistant_text = ''
-        continue_text = ''
-        last_assistant_text = ''
         messages = []
         continue_message = []
         
-        def add_continue_message():
-            nonlocal continue_text
-            if continue_text:
-                continue_message.append([
-                    {'role': role, 'content': '.'},
-                    {'role': 'user', 'content': continue_text},
-                ])
-                continue_text = ''
-
         for line in p.split('\n'):
-            if line.lower() == 'continue:' or line.lower() == 'continue-chain:':
-                if assistant_text:
-                    messages.append({'role': role, 'content': assistant_text})
-                if user_text:
-                    messages.append({'role': role, 'content': user_text})
-                if continue_text:
-                    add_continue_message()
-                assistant_text = ''
-                user_text = ''
-                continue_text = ''
-                role = 'continue' if line.lower() == 'continue:' else 'continue-chain'
+            line = line.strip()
+            if line.lower().startswith('to_context:'):
+                context_name = line.lower().split(':')[1].strip()
+                continue_message.append([
+                    {'role': 'to_context', 'content': context_name},
+                ])
                 continue 
             if line.lower() ==  'user:':
-                if continue_text:
-                    add_continue_message()
                 if assistant_text:
                     messages.append({'role': role, 'content': assistant_text})
                 if user_text:
@@ -120,8 +103,6 @@ def generate_text(repo_id: str,
                 role = 'user'
                 continue
             if line.lower() == 'assistant:':
-                if continue_text:
-                    add_continue_message()
                 if user_text:
                     messages.append({'role': role, 'content': user_text})
                     user_text = ''
@@ -130,9 +111,8 @@ def generate_text(repo_id: str,
                 assistant_text = ''
                 role = 'assistant'
                 continue
-            if role == 'continue' or role == 'continue-chain':
-                continue_text += '\n' + line
-            elif role == 'assistant':
+
+            if role == 'assistant':
                 assistant_text += '\n' + line
             elif role == 'user':
                 user_text += '\n' + line
@@ -141,9 +121,8 @@ def generate_text(repo_id: str,
             messages.append({'role': role, 'content': user_text})
         if assistant_text:
             messages.append({'role': role, 'content': assistant_text})
-        if continue_text:
-            add_continue_message()
         message_texts.append(messages)
+        
         if continue_message:
             message_texts += continue_message
             continue_message = []
@@ -152,45 +131,45 @@ def generate_text(repo_id: str,
     
     ProgressBar.set_title('[chatvision] - describing the image')
     with torch.no_grad():
-        for i, img in enumerate(image):
+        for i, src_img in enumerate(image):
             ProgressBar.set_progress(i * 100.0/ len(image))
             turn_result = []
-            last_response_continue = ''
             last_response = ''
             for msgs in message_texts:
-                continue_role = ''
-                if msgs[0]['role'] in ('continue', 'continue-chain'):
-                    if not last_response_continue:
-                        continue
-                    continue_content =  msgs[1]['content']
-                    if '<to_context:' in continue_content:
-                        start = continue_content.index('<to_context:')
-                        end = continue_content.index('>', start)
+                should_skip = False
+                ignore_image = False
+                img = src_img
+                for msg in msgs:
+                    if '<noimg>' in msg['content']:
+                        img = None
+                        ignore_image = True
+                        msg['content'] = msg['content'].replace('<noimg>', '')
+                    if '<skip>' in msg['content']:
+                        should_skip = True
+                        break
+                        
+                    while '<from_context:' in msg['content']:
+                        start = msg['content'].index('<from_context:')
+                        end = msg['content'].index('>', start)
                         if end == -1:
                             print('Error: missing ">" ')
                             break
-                        context_name = continue_content[start + 12:end].lower()
-                        context_dict[context_name] = last_response_continue
-                        continue    
-                    continue_role = msgs[0]['role'] != 'continue-chain'
-                    msgs[0]['role'] = 'assistant'
-                    msgs[0]['content'] = last_response_continue
-                if last_response:
-                    for msg in msgs:
-                        if '<from_assistant>' in msg['content']:
-                            msg['content'] = msg['content'].replace('<from_assistant>', last_response)
-                        if '<from_context' in msg['content']:
-                            start = msg['content'].index('<from_context')
-                            end = msg['content'].index('>', start)
-                            if end == -1:
-                                print('Error: missing ">" ')
-                                break
-                            context_name = msg['content'][start + 13:end].lower()
-                            if context_name in context_dict:
-                                msg['content'] = msg['content'][:start] + context_dict[context_name] + msg['content'][end + 1:]
-                            else:
-                                msg['content'] = msg['content'][:start] + msg['content'][end + 1:]
-                                
+                        context_name = msg['content'][start + 14:end].lower()
+                        if context_name in context_dict:
+                            msg['content'] = msg['content'][:start] + context_dict[context_name] + msg['content'][end + 1:]
+                        else:
+                            msg['content'] = msg['content'][:start] + msg['content'][end + 1:]                    
+
+                if should_skip:
+                    continue
+                
+                continue_role = ''
+                if msgs[0]['role'] == 'to_context':
+                    if last_response:
+                        context_name =  msgs[0]['content']
+                        context_dict[context_name] = last_response
+                    continue
+                    
                 response = chatvision_model.model.chat(
                     image=img,
                     msgs=msgs,
@@ -201,15 +180,16 @@ def generate_text(repo_id: str,
                     temperature=temperature,
                     system_prompt=system_prompt
                 )
-                if not continue_role:
-                    last_response_continue = response
+                
                 last_response = response
                 turn_result.append(response)
+                
             for index, response in enumerate(turn_result):
                 response = response.split('\n')
                 response = [r.strip() for r in response if r.strip()]
                 response = '\n'.join([r for r in response if r])
                 turn_result[index] = response
+
             responses.append('\n\n'.join(turn_result))
 
     return {
